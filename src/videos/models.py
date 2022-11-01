0f1c2447ee29039adfaea838e04b3c152fc2e3af
unnamedKnight <backfrontdev47@gmail.com>
2022-11-01 22:13:40 +0600

    Add video model

--- src/videos/models.py
+++ src/videos/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.utils import timezone
+
 
 # Create your models here.
+
+class VideoQuerySet(models.QuerySet):
+    def published(self):
+        now = timezone.now()
+        return self.filter(
+            state=Video.PublishStateOptions.PUBLISH,
+            publish_timestamp__lte=now
+        )
+
+
+class VideoManager(models.Manager):
+    def get_queryset(self):
+        return VideoQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
+
+
+class Video(models.Model):
+    class PublishStateOptions(models.TextChoices):
+        PUBLISH = 'PU', 'Publish'
+        DRAFT = 'DR', 'Draft'
+
+    title = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True)  # 'this-is-my-video'
+    video_id = models.CharField(max_length=220, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    state = models.CharField(max_length=2, choices=PublishStateOptions.choices, default=PublishStateOptions.DRAFT)
+    publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
+
+    objects = VideoManager()
+
+    @property
+    def is_published(self):
+        return self.active
+
+    # def get_playlist_ids(self):
+    #     # self.<foreigned_obj>_set.all()
+    #     return list(self.playlist_featured.all().values_list('id', flat=True))
+
+
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'All Video'
+        verbose_name_plural = 'All Videos'
+
+
+class VideoPublishedProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = 'Published Videos'
+
+
+def publish_state_pre_save(sender, instance, *args, **kwargs):
+    is_publish = instance.state == instance.PublishStateOptions.PUBLISH
+    is_draft = instance.state == instance.PublishStateOptions.DRAFT
+    if is_publish and instance.publish_timestamp is None:
+        instance.publish_timestamp = timezone.now()
+    elif is_draft:
+        instance.publish_timestamp = None
+
+
+pre_save.connect(publish_state_pre_save, sender=Video)
