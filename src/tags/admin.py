2f4a0141ca67966cf5b062ea4e5c4a4166f5a014
unnamedKnight <backfrontdev47@gmail.com>
2022-11-03 22:24:03 +0600

    Add db.json

--- src/tags/admin.py
+++ src/tags/admin.py
@@ -1,3 +1,20 @@
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.contrib import admin
 
-# Register your models here.
+from .models import TaggedItem
+
+
+class TaggedItemInline(GenericTabularInline):  # admin.TabularInline
+    model = TaggedItem
+    extra = 0
+
+
+class TaggedItemAdmin(admin.ModelAdmin):
+    fields = ['tag', 'content_type', 'object_id', 'content_object']
+    readonly_fields = ['content_object']
+
+    class Meta:
+        model = TaggedItem
+
+
+admin.site.register(TaggedItem, TaggedItemAdmin)
--- src/tags/models.py
+++ src/tags/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 # Create your models here.
+
+class TaggedItem(models.Model):
+    tag = models.SlugField(null=True, blank=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    # def get_related_object(self):
+    #     model_class = self.content_type.model_class()
+    #     return model_class.objects.get(id=self.object_id)
+
+
