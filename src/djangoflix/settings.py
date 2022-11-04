2c2d6e3523aa9b28259b45245f10051686be2523
unnamedKnight <backfrontdev47@gmail.com>
2022-11-04 10:43:55 +0600

    Add ratings app

--- src/djangoflix/settings.py
+++ src/djangoflix/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'playlists.apps.PlaylistsConfig',
     'categories.apps.CategoriesConfig',
     'tags.apps.TagsConfig',
+    'ratings.apps.RatingsConfig',
 ]
 
 MIDDLEWARE = [
